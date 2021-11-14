2dfaff423faf3929a5ed97945f7597bc17a4c9eb
nidhiparikh5294 <nidhi.parikh5@gmail.com>
2021-11-14 15:10:34 -0500

    Adding css and setting validations for 0 and 1 value

---
+++ src/app/primenumberchart/countnumber.service.ts
@@ -0,0 +1,60 @@
+import { DecimalPipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountnumberService {
+
+  constructor(private decimalPipe: DecimalPipe) { }
+  /* Core Logic for counting primes from range */
+  countPrimeNumber(rangeNumber: number): any {
+    /* Add observables for synchronous execution */
+    return new Observable((observer) => {
+      let totalCount = 0;
+      /* Taking start time for time difference */
+      var startTime = performance.now();
+      /* Looping from 2 to range number */
+      for (let i = 2; i <= rangeNumber; i++) {
+        /* checking current number is prime or not */
+        if (this.isPrime(i)) {
+          totalCount++;
+          console.log(totalCount);
+        }
+      }
+      /* Taking end time for time difference */
+      var endTime = performance.now();
+      var timeDifferance: any = endTime - startTime;
+      /*  Count Milli seconds or seconds */
+      var totalTime = this.millisToSeconds(timeDifferance);
+      /*  Setting observables value */
+      observer.next({ 'totalCount': totalCount, 'timeDifferance': totalTime });
+      observer.complete();
+    });
+  }
+  /* Function for counting prime numbers */
+  isPrime(currentNumber: number): boolean {
+    for (let i = 2; i < currentNumber; i++)
+      if (currentNumber % i === 0) return false;
+    return true;
+  }
+
+  /* Function for counting milliseconds and seconds */
+  millisToSeconds(milliSeconds: any): any {
+    /* Converting to Milli Seconds */
+    var seconds: any = ((milliSeconds % 60000) / 1000).toFixed(0);
+    if (((seconds < 10 ? '0' : '') + seconds) == '00') {
+      return this.pad(milliSeconds) + ' mS';
+    }
+    /** Converting to Seconds */
+    else {
+      seconds = ((milliSeconds % 60000) / 1000).toFixed(2);
+      return (seconds < 10 ? '0' : '') + seconds + ' S';
+    }
+  }
+  /* Function for trimming digits after precision */
+  pad(milliSeconds: any): any {
+    return this.decimalPipe.transform(milliSeconds, '.2-2');
+  }
+}
--- src/app/primenumberchart/primenumberchart.component.ts
+++ src/app/primenumberchart/primenumberchart.component.ts
@@ -1,66 +1,81 @@
 import { DecimalPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CountnumberService } from './countnumber.service';
 
 @Component({
   selector: 'app-primenumberchart',
   templateUrl: './primenumberchart.component.html',
   styleUrls: ['./primenumberchart.component.scss']
 })
+
+/*  Count Prime number from 1 to N **/
+
 export class PrimenumberchartComponent implements OnInit {
   rangeNumber: number = 1;
   totalCount: number = 0;
   totalTime: string = '';
   isCounting: boolean = false;
+  isOne: boolean = false;
   countLabel: string = 'Count Prime Numbers';
-  countingLabel = 'Prime Numbers are counting...'
-
-  constructor(private decimalPipe: DecimalPipe) { }
+  countingLabel = 'Processing...';
+  projectLable: string = 'Prime Numbers Count';
+  rangleLable: string = 'Enter  Number';
+  oneLabel: string = 'Enter number greaterthan 1.';
+  checkPrimeNumberLable: string = 'Checking Primes from 1 to ';
+  primeCountLable: string = 'Prime Count';
+  runTimeLable: string = 'Run Time';
+  // countDigits: number = 0;
+  /* Inject services */
+  constructor(private countnumberService: CountnumberService) { }
 
   ngOnInit(): void {
   }
 
+  /* Count Prime Numbers from 1 To N */
   countPrimeNumber(): void {
-    let count = 0;
-    this.isCounting = true;
-    var startTime = performance.now();
-    for (let i = 1; i < this.rangeNumber; i++) {
-      if (this.isPrime(i)) {
-        count++;
-        console.log(count);
-      }
+    /* Checking if number is 1 or 0 */
+    if (this.rangeNumber == 1 || this.rangeNumber == 0) {
+      this.isOne = true;
     }
-    this.totalCount = count;
-    var endTime = performance.now();
-    var timeDifferance: any = endTime - startTime;
-
-    this.totalTime = this.millisToSeconds(timeDifferance);
-    this.isCounting = false;
-
-
-  }
-
-  isPrime(currentNumber: number): boolean {
-    for (let i = 2; i < currentNumber; i++)
-      if (currentNumber % i === 0) return false;
-    return currentNumber > 1;
-  }
-
-  millisToSeconds(milliSeconds: any): any {
-
-    var seconds: any = ((milliSeconds % 60000) / 1000).toFixed(2);
-    if (((seconds < 10 ? '0' : '') + seconds) == '00') {
-      return this.pad(milliSeconds) + ' mS';
-    } else {
-      return (seconds < 10 ? '0' : '') + seconds + ' S';
+    /* Counting primes  */
+    else {
+      /* Enables falgs */
+      let oldLabel = this.countLabel;
+      this.isCounting = true;
+      this.countLabel = this.countingLabel;
+      /* Call service for count prime numbers */
+      this.countnumberService.countPrimeNumber(this.rangeNumber)
+        .subscribe({
+          next: (response: any) => {
+            /* Setting response to component variables */
+            this.totalCount = response.totalCount;
+            this.totalTime = response.timeDifferance;
+            this.isCounting = false;
+            this.isOne = false;
+            this.countLabel = oldLabel;
+            // this.countDigits = 0;
+          },
+          /* Checking errors */
+          error: (error: any) => {
+            this.isCounting = false;
+            console.log(error);
+          }
+        });
     }
   }
-  pad(milliSeconds: any): any {
-    return this.decimalPipe.transform(milliSeconds, '.2-2');
-  }
-
+  /*  On model change cheacking if number is changed or not */
   rangeNumberChanged(event: any) {
     this.totalCount = 0;
   }
-
+  /* Checking Number are >= 1*/
+  isNumberKey(event: any) {
+    var charCode = (event.which) ? event.which : event.keyCode;
+    // if (charCode == 48 && this.countDigits == 0)
+    //   return false;
+    // else {
+    //   this.countDigits++;
+    return !(charCode > 31 && (charCode < 48 || charCode > 57));
+    // }
+  }
 
 }
